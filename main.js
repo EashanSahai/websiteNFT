cf313f5d49169450d91dbdc4e361fbc7ad03da25
EASHAN <eashansahai@gmail.com>
2024-05-06 14:44:13 -0600

    Update main.js

--- main.js
+++ main.js
@@ -161,7 +161,7 @@ function drawCircuitBoxesSequentially(elements) {
 
     // Draw the transition to the next path starting point
     function drawTransitionToNextPath(nextStart) {
-        const lastPoint = persistentPath[persistentPath.length - 3];
+        const lastPoint = persistentPath[persistentPath.length - 10];
         connectorPath.push({ x: lastPoint.x - 100, y: lastPoint.y});
         connectorPath.push({ x: lastPoint.x - 100, y: nextStart.y - 35 });
         connectorPath.push({ x: nextStart.x, y: nextStart.y - 35 });
